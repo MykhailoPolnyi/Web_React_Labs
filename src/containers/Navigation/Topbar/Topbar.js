2d14057062c95009fbd5679c5b1c564734992d50
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-11-08 02:05:27 +0200

    Changed navbar directory

--- src/containers/Navigation/Topbar/Topbar.js
+++ src/containers/Navigation/Topbar/Topbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {TopbarStyled} from "./Topbar.styled";
-import {Navbar} from "../../../components/Navbar/Navbar";
+import {Navbar} from "../Navbar/Navbar";
 import {Logo} from "../../../components/Logo/Logo";
 
 export const Topbar = () => (

f8a13cb260c71dd44e203bc5b9c6fc45b4268362
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-12-04 18:48:36 +0200

    Added price counter to cart

--- src/containers/Pages/Cart/Cart.js
+++ src/containers/Pages/Cart/Cart.js
@@ -4,11 +4,14 @@ import {useSelector, useDispatch} from "react-redux";
 import {removeFish, clearCart} from "../../../app/storage/features/fishCart/fishSlice";
 import {getFishCards} from "../../../components/Card/CardStorage/StorageDom";
 import {DeleteButtonStyled} from "../../../components/Buttons/DeleteButton.styled";
+import {ActionPanelStyled} from "../../../components/ActionPannel/ActionPanel.styled";
 
 export const Cart = () => {
     const fishList = useSelector((state) => state.fishInCart.value)
+    const totalPrice = useSelector((state) =>
+        state.fishInCart.value.reduce((total, fish) => (total + fish.price), 0)
+    )
     const dispatch = useDispatch();
-
 
     if (fishList.length === 0) {
         return (<h1>Cart is empty</h1>)
@@ -16,11 +19,15 @@ export const Cart = () => {
 
     return (
         <PageStyled style={{height:"100%"}}>
-            <DeleteButtonStyled onClick={() => {dispatch(clearCart())}}>
-                Clear cart
-            </DeleteButtonStyled>
+            <ActionPanelStyled>
+                <h2>Total price: {totalPrice}</h2>
+                <DeleteButtonStyled onClick={() => {dispatch(clearCart())}}>
+                    Clear cart
+                </DeleteButtonStyled>
+            </ActionPanelStyled>
             <div style={{display:"flex", flexDirection:"row"}}>
                 {
+
                     getFishCards(fishList, (fish) =>
                         (<DeleteButtonStyled onClick={() => {dispatch(removeFish(fish.id))}}>
                             Remove
